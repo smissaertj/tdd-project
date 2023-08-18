c4eaed7330b2f5dd5d9d2709799f14dcbffa9711
Joeri Smissaert <smissaertj@gmail.com>
2023-08-18 14:58:27 +0400

    feat: conversion of Money from EUR to USD

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,9 +7,17 @@ class Portfolio {
     this.moneys = [...this.moneys, ...moneys] // this.moneys.concat(moneys)
   }
 
+  convert(money, currency){
+    let eurToUSD = 1.2
+    if(money.currency === currency){
+      return money.amount
+    }
+    return money.amount * eurToUSD
+  }
+
   evaluate(currency){
     let total = this.moneys.reduce( (sum, money) => {
-      return sum + money.amount
+      return sum + this.convert(money, currency)
     }, 0)
     return new Money(total, currency)
   }
